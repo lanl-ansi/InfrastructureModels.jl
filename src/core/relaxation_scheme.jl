0cac838fd07d65b3dbcd6173fc8116018a245ed5
ccoffrin <carleton@coffrin.com>
2019-01-30 15:21:12 -0500

    restore SOC form

--- src/core/relaxation_scheme.jl
+++ src/core/relaxation_scheme.jl
@@ -7,7 +7,8 @@ end
 "a conic encoding of constraint: `c^2 + d^2 <= a*b`"
 function relaxation_complex_product_conic(m, a, b, c, d)
     @assert (JuMP.lower_bound(a) >= 0 && JuMP.lower_bound(b) >= 0) || (JuMP.upper_bound(a) <= 0 && JuMP.upper_bound(b) <= 0)
-    JuMP.@constraint(m, [a, b, c, d] in JuMP.RotatedSecondOrderCone())
+    #JuMP.@constraint(m, [a, b, c, d] in JuMP.RotatedSecondOrderCone())
+    JuMP.@constraint(m, [a+b, a-b, c, d] in JuMP.SecondOrderCone())
 end
 
 """
