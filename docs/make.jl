a8647159146c23df638a9a358bef24317e117811
carleton <cjc@lanl.gov>
2019-08-16 17:48:56 -0600

    fixes for latest version of documenter

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,6 @@ makedocs(
     format = :html,
     sitename = "InfrastructureModels",
     authors = "Carleton Coffrin, Russell Bent, and contributors",
-    #analytics = "UA-367975-10",
     pages = [
         "Home" => "index.md",
         "Library" => "library.md",
@@ -14,9 +13,5 @@ makedocs(
 )
 
 deploydocs(
-    deps = nothing,
-    make = nothing,
-    target = "build",
     repo = "github.com/lanl-ansi/InfrastructureModels.jl.git",
-    julia = "1.1"
 )
