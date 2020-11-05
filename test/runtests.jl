69a04bec919e098da09d59e4662d1b321ac6e52a
Byron Tasseff <btasseff@lanl.gov>
2020-11-04 17:57:28 -0700

    Suppress test warnings.

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,12 +7,13 @@ import JuMP
 import Ipopt
 import ECOS
 import Juniper
+import Logging
 
 const MOI = JuMP.MathOptInterface
 
-ipopt_solver = JuMP.with_optimizer(Ipopt.Optimizer, print_level=0)
-ecos_solver = JuMP.with_optimizer(ECOS.Optimizer, verbose=0)
-juniper_solver = JuMP.with_optimizer(Juniper.Optimizer, nl_solver=ipopt_solver, log_levels=[])
+ipopt_solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0)
+ecos_solver = JuMP.optimizer_with_attributes(ECOS.Optimizer, "verbose" => 0)
+juniper_solver = JuMP.optimizer_with_attributes(Juniper.Optimizer, "nl_solver" => ipopt_solver, "log_levels" => [])
 
 import Random: seed!
 
@@ -20,6 +21,8 @@ using Test
 
 # Suppress warnings during testing
 InfrastructureModels.logger_config!("error")
+Logging.disable_logging(Logging.Info)
+Logging.disable_logging(Logging.Warn)
 
 include("common.jl")
 
